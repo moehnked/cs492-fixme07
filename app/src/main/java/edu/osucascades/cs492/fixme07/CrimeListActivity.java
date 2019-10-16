1c42f36dacbadc294e9c115221090dd7d7a46dcd
chaneyr <jesse.chaney@osucascades.edu>
2019-10-16 06:38:10 -0700

    more commits

---
+++ app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package edu.osucascades.cs492.fixme07;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package edu.osucascades.cs492.fixme07;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+}
